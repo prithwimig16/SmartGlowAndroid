666021c46eb41cefcd48081eb15d576632ee8a90
DESKTOP-IJD71UV\prith <prithwi3frameslab>
2018-05-17 12:00:48 +0530

    Initial commit

---
+++ app/src/main/java/tfl/smartglow/BlueTooth/BlueToothActivity.java
@@ -0,0 +1,17 @@
+package tfl.smartglow.BlueTooth;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tfl.smartglow.R;
+
+public class BlueToothActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue_tooth);
+    }
+
+}
---
+++ app/src/main/java/tfl/smartglow/WelcomeHome/WelcomeHomeActivity.java
@@ -0,0 +1,15 @@
+package tfl.smartglow.WelcomeHome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tfl.smartglow.R;
+
+public class WelcomeHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_home);
+    }
+}
